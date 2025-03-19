a1d70c891f08709bc072f22cdc7d0699fdcb0703
Spencer Porter <spencer.porter3@gmail.com>
2025-03-18 21:05:16 -0400

    Update documentation and improve docstrings for Toolkit class

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vaul",
-    version="0.2.1",
+    version="0.2.4",
     description="A lightweight Python library for building agentic actions and workflows.",
     author="Spencer Porter",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
         'pydantic==2.6.4',
+        'pandas>=2.0.0',
     ],
     packages=find_packages(),
     python_requires='>=3.6',
